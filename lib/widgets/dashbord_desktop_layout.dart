7dbb731b997150a9b5bf678bd472b09041b47bf5
mostafanagy0 <mostafanagy855@gmail.com>
2025-01-22 20:50:37 +0200

    income section

--- lib/widgets/dashbord_desktop_layout.dart
+++ lib/widgets/dashbord_desktop_layout.dart
@@ -29,3 +29,4 @@ class DashBordDesktopLayout extends StatelessWidget {
     );
   }
 }
+     
---
+++ lib/widgets/income_section.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:resposive_dashbord/widgets/custom_background_container.dart';
+import 'package:resposive_dashbord/widgets/income_section_header.dart';
+
+class IncomeSection extends StatelessWidget {
+  const IncomeSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomBackgroundContainer(
+        child: Column(
+      children: [
+        IncomeSectionHeader(),
+      ],
+    ));
+  }
+}
